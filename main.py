e6a20b768da202a553c3de1161c7b75b82ffc7b9
rajkumaryadav19912111-star <rajkumaryadav19912111@gmail.com>
2026-03-21 18:41:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import requests
+from datetime import datetime
+from telegram import Bot
+import os
 
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+bot = Bot(token=TOKEN)
+
+symbols = ["BTCUSDT", "ETHUSDT"]
+
+def get_price(symbol):
+    url = f"https://fapi.binance.com/fapi/v1/ticker/price?symbol={symbol}"
+    return float(requests.get(url).json()["price"])
+
+print("Bot Running...")
+
+for sym in symbols:
+    price = get_price(sym)
+
+    msg = f"""
+🚀 Crypto Alert
+
+Coin: {sym}
+Price: {price}
+Time: {datetime.now().strftime("%H:%M:%S")}
+"""
+
+    bot.send_message(chat_id=CHAT_ID, text=msg)
